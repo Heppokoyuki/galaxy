8514c7f672d991c72bc32c77cca590dc03837744
Yuki Azuma <yuhki.yasuda@gmail.com>
2023-01-18 08:14:24 +0900

    sync

--- nbody/main.cpp
+++ nbody/main.cpp
@@ -8,6 +8,7 @@
 #include <omp.h>
 #include <sstream>
 #include <chrono>
+#include <immintrin.h>
 
 // #define DEBUG_TIME
 
@@ -17,10 +18,11 @@ const double MAX_T = 10;
 
 using namespace std;
 
-struct particle {
-    double m;
-    double x[3];
-    double v[3];
+union particle {
+    float f[8];
+    struct  {
+        float x, y, z, m, vx, vy, vz, eps2;
+    } __attribute__((aligned(32)));
 };
 
 string
@@ -47,12 +49,13 @@ printParts(const vector<particle> &parts, ostream &st)
 {
     for(int i = 0; i < parts.size(); ++i) {
         st << scientific << setprecision(5)
-             << parts[i].m << " " << parts[i].x[0] << " " << parts[i].x[1] << " " << parts[i].x[2] << " "
-                                  << parts[i].v[0] << " " << parts[i].v[1] << " " << parts[i].v[2] << endl;
+             << parts[i].m << " " << parts[i].x << " " << parts[i].y << " " << parts[i].z << " "
+                                  << parts[i].vx << " " << parts[i].vy << " " << parts[i].vz << endl;
     }
 }
 
-int main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
     vector<string> args(argv, argv + argc);
     size_t N, count;
@@ -61,18 +64,34 @@ int main(int argc, char *argv[])
     ifstream ifs(input_file_name);
     ofstream ofs(createFileName(input_file_name));
 
+    /* Hello SIMD Registers */
+    __m256 XI, YI, ZI, EPS2;
+    __m256 XJ, YJ, ZJ, MJ, RJ;
+
+    RJ = _mm256_zeroall();
+
+    __m256 lift1, lift2;
+    __m256 DX, DY, DZ;
+    __m256 PHI;
+    __m256 AX, AY, AZ;
+
+    PHI = _mm256_zeroall();
+
     ifs >> N >> init_t;
+    if(N % 8 != 0) {
+        cerr << "N is not correct!" << endl;
+        return -1;
+    }
 
     vector<particle> parts(N);
-    double distance;
 
     ofs << scientific << setprecision(8);
 
     dt = 1000;
     for(int i = 0; i < N; ++i) {
-        double m, x, y, z, vx, vy, vz;
+        float m, x, y, z, vx, vy, vz;
         ifs >> m >> x >> y >> z >> vx >> vy >> vz;
-        parts[i] = {m, {x, y, z}, {vx, vy, vz}};
+        parts[i] = {m, x, y, z, vx, vy, vz, eps2};
         dt = min(dt, eps / sqrt(vx*vx + vy*vy + vz*vz));
     }
 
@@ -83,54 +102,80 @@ int main(int argc, char *argv[])
     /* print initial info */
     ofs << t << endl;
     printParts(parts, ofs);
-    
-    omp_set_num_threads(stoi(args.at(1)));
-    #pragma omp parallel for
-    for(int idx = 0; idx < N; ++idx) {
-        for(int base = 0; base < 3; ++base) {
-            parts[idx].x[base] = parts[idx].x[base] + parts[idx].v[base] * 0.5 * dt;
+
+    for(int i = 0; i < N; i += 4) {
+        XI = _mm256_set_ps(
+                    parts.at(i+3).x,
+                    parts.at(i+2).x,
+                    parts.at(i+1).x,
+                    parts.at(i+0).x,
+                    parts.at(i+3).x,
+                    parts.at(i+2).x,
+                    parts.at(i+1).x,
+                    parts.at(i+0).x
+             );
+        YI = _mm256_set_ps(
+                    parts.at(i+3).y,
+                    parts.at(i+2).y,
+                    parts.at(i+1).y,
+                    parts.at(i+0).y,
+                    parts.at(i+3).y,
+                    parts.at(i+2).y,
+                    parts.at(i+1).y,
+                    parts.at(i+0).y
+             );
+        ZI = _mm256_set_ps(
+                    parts.at(i+3).z,
+                    parts.at(i+2).z,
+                    parts.at(i+1).z,
+                    parts.at(i+0).z,
+                    parts.at(i+3).z,
+                    parts.at(i+2).z,
+                    parts.at(i+1).z,
+                    parts.at(i+0).z
+             );
+        EPS2 = _mm256_set_ps(
+                    parts.at(i+3).eps2,
+                    parts.at(i+2).eps2,
+                    parts.at(i+1).eps2,
+                    parts.at(i+0).eps2,
+                    parts.at(i+3).eps2,
+                    parts.at(i+2).eps2,
+                    parts.at(i+1).eps2,
+                    parts.at(i+0).eps2
+              );
+        for(int j = 0; j < N; j += 2) {
+            lift1 = _mm256_load_ps(parts.at(j).f);
+            lift2 = _mm256_load_ps(parts.at(j+1).f);
+
+            /* XJ: MSB {{xj1, yj1, zj1, mj1}, {xj0, yj0, zj0, mj0}} */
+            XJ = _mm256_insertf128_ps(lift1, _mm256_castps256_ps128(lift2), 1);
+
+            MJ = _mm256_shuffle_ps(XJ, XJ, 0b00000000);
+            YJ = _mm256_shuffle_ps(XJ, XJ, 0b10101010);
+            ZJ = _mm256_shuffle_ps(XJ, XJ, 0b01010101);
+            XJ = _mm256_shuffle_ps(XJ, XJ, 0b11111111);
+
+            DX = _mm256_sub_ps(XJ, XI);
+            DY = _mm256_sub_ps(YJ, YI);
+            DZ = _mm256_sub_ps(ZJ, ZI);
+
+            /* r^2 = dx^2+dy^2+dz^2+eps^2 */
+            RJ = _mm256_fmadd_ps(DX, DX, RJ);
+            RJ = _mm256_fmadd_ps(DY, DY, RJ);
+            RJ = _mm256_fmadd_ps(DZ, DZ, RJ);
+            RJ = _mm256_add_ps(RJ, EPS2);
+
+            /* culculate m/r^3 */
+            RJ = _mm256_rsqrt_ps(RJ);
+            MJ = _mm256_mul_ps(RJ, MJ);
+            RJ = _mm256_mul_ps(RJ, RJ);
+            RJ = _mm256_mul_ps(RJ, MJ);
+
+            DX = _mm256_mul_ps(DX, RJ);
+            DY = _mm256_mul_ps(DY, RJ);
+            DZ = _mm256_mul_ps(DZ, RJ);
+
         }
     }
-
-    while(t < MAX_T) {
-#ifdef DEBUG_TIME
-        auto start = chrono::system_clock::now();
-#endif
-        cout << "steps: " << count << endl;
-
-        #pragma omp parallel for
-        for(int i = 0; i < N; ++i) {
-            for(int j = 0; j < N; ++j) {
-                double dx = parts[j].x[0] - parts[i].x[0];
-                double dy = parts[j].x[1] - parts[i].x[1];
-                double dz = parts[j].x[2] - parts[i].x[2];
-                double rsq = dx*dx + dy*dy + dz*dz;
-                double mrinv3 = parts[j].m * pow(rsq + eps2, -1.5) * dt;
-
-                parts[i].v[0] += dx * mrinv3;
-                parts[i].v[1] += dy * mrinv3;
-                parts[i].v[2] += dz * mrinv3;
-            }
-        }
-
-        #pragma omp parallel for
-        for(int idx = 0; idx < N; ++idx) {
-            for(int base = 0; base < 3; ++base) {
-                parts[idx].x[base] = parts[idx].x[base] + parts[idx].v[base] * dt;
-            }
-        }
-
-        t += dt;
-        count++;
-        if(count % 10 == 0) {
-            ofs << t << endl;
-            printParts(parts, ofs);
-        }
-#ifdef DEBUG_TIME
-        auto end = chrono::system_clock::now();
-        cout << "time: " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << endl;
-#endif
-    }
-    cout << "total run: " << count << endl;
-    return 0;
 }
